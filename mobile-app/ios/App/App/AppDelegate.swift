e6e1a85a5bd8dc99a7ae3c2d0bceebf6dd6f5e5d
zeros1205 <jax1205@gmail.com>
2026-05-18 23:08:49 +0900

    fix(ios/firebase): GoogleService-Info.plist 번들 누락 SIGABRT 크래시 수정 (#110)
    - 워크플로우 Configure App target signing 스텝의 ruby+xcodeproj 블록에 GoogleService-Info.plist 자동 등록 로직 추가 (멱등)
    - AppDelegate.swift: plist 누락 시 FirebaseApp.configure() 건너뛰기 방어 코드
    - 빌드 1.0.1(4) SIGABRT 크래시(FIRApp configure 실패) 해결
    - Android 영향 없음

--- mobile-app/ios/App/App/AppDelegate.swift
+++ mobile-app/ios/App/App/AppDelegate.swift
@@ -14,7 +14,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         #if canImport(FirebaseCore)
-        if FirebaseApp.app() == nil {
+        if FirebaseApp.app() == nil,
+           Bundle.main.path(forResource: "GoogleService-Info", ofType: "plist") != nil {
             FirebaseApp.configure()
         }
         #endif
